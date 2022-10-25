24becdf62e37305106aa9f3633734c659a27d951
MhSolyman <solymanbijoye.com>
2022-10-25 20:03:51 +0600

    add fake data

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 const dataOne=require('./data/data.json')
+
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('runnig api')
